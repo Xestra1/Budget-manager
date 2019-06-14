214b786b06f64519fc3752c49de6505bc257980c
Xestra1 <33702536+Xestra1@users.noreply.github.com>
2019-06-14 14:30:10 -0400

    added some stuff
    - fix remove

--- src/js/app.js
+++ src/js/app.js
@@ -2,32 +2,45 @@ var balance = 0;
 var loses = 0;
 var profit = 0;
 var num = 0;
+document.getElementById("totalAmount").innerHTML = balance;
 
 function setBal() {
     balance = document.getElementById("setBal").value;
     document.getElementById("totalAmount").innerHTML = balance;
-    console.log(balance);
 }
 
 function makeItem() {
+    if (document.getElementById('name').value == '' || document.getElementById('description').value == '' || document.getElementById('cost').value == '') {
+        alert("Please fill in all inputs");
+        return "error";
+    }
     var z = document.createElement('div');
+    var name = document.getElementById('name').value;
+    var desc = document.getElementById('description').value;
+    var cost = document.getElementById('cost').value;
     z.innerHTML =
         `<div class="item" id="${num}">
         <hr>
         <div class="f-l">
-            <h6 id="name${num}">Name</h6>
-            <p id="desc${num}">Desc</p>
+            <h6 id="name${num}">${name}</h6>
+            <p id="desc${num}">${desc}</p>
         </div>
         <div class="f-r">
-            <i id="remove${num}" class="fas fa-times"></i>
-            <p id="cost${num}">cost</p>
+            <a id="remove${num}" onclick="deleteItem(${num})" class="fas fa-times"></a>
+            <p id="cost${num}">$${cost}</p>
         </div>
         <hr>
     </div>`;
     document.getElementById("items").appendChild(z);
+    document.getElementById('name').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('cost').value = '';
     num++;
+
+    loses = cost - balance;
+    console.log(loses);
 }
 
-makeItem();
-makeItem();
-makeItem();
+function deleteItem(num) {
+    document.getElementById("items").removeChild(num);
+}
